8f7864d3a9a12a38077c6c17a701757e14ef027b
Андрей Смирнов <andrejsmirnov@MacBook-Air-Andrej.local>
2026-04-16 08:52:07 +0300

    9100

--- src/test/java/tes/proba.java
+++ src/test/java/tes/proba.java
@@ -19,7 +19,7 @@ public class proba {
         sleep(2000);
         $("a[href=\"/selenide/selenide/wiki/SoftAssertions\"").click();
         $("body").shouldHave(text("Using JUnit5 extend test class"));
-        sleep(9000);
+        sleep(9100);
 
     }
 }
